966440a2ac2106c0e52add0414cec7cb55270ad1
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-08 11:03:08 -0700

    xxx

---
+++ public/bootstrap-test.ps1
@@ -0,0 +1,21 @@
+#!/usr/bin/env pwsh
+
+$securePassword = Read-Host -Prompt 'Password' -AsSecureString
+$password = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+    [Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword)
+)
+
+# Use integer value for TLS 1.2 to support .NET 4.0
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]3072
+
+$creds = [Convert]::ToBase64String(
+    [Text.Encoding]::UTF8.GetBytes(":$password")
+)
+
+$url = 'https://dotfiles-init-test.drubermann.workers.dev/.init_script.ps1'
+
+$webClient = New-Object System.Net.WebClient
+$webClient.Headers.Add('Authorization', "Basic $creds")
+$script = $webClient.DownloadString($url)
+
+Invoke-Expression $script
