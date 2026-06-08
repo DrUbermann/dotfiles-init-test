d2d85e1609ee9660c2fe4ec2eefb2b963296b88f
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-08 13:56:02 -0700

    xxx

--- init-test.ps1
+++ init-test.ps1
@@ -7,6 +7,8 @@ Set-ExecutionPolicy -Scope CurrentUser Bypass
 # Use integer value for TLS 1.2 to support .NET 4.0
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]3072
 
+Set-Location ~
+
 iex "&{$(irm 'https://get.chezmoi.io/ps1')} init --apply 'https://github.com/DrUbermann/dotfiles-init-test.git'"
 #$Env:LGR_LVL_CNSL = 0
 & "$HOME/chezmoi.tmp/init.ps1"
