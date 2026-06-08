7e5d8f3cc4f507a4409918e9122f90958209e0d4
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-07 21:33:27 -0700

    xxx

--- public/bootstrap-test.sh
+++ public/bootstrap-test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo starting
+
 ## prompt for password without echoing it
 printf 'Password: ' > /dev/tty
 stty -echo < /dev/tty
@@ -10,7 +10,7 @@ printf '\n' > /dev/tty
 _creds=$(printf ':%s' "$_password" | base64)
 #_header="--header Authorization: Basic $_creds"
 _url="https://dotfiles-init-test.drubermann.workers.dev/.init_script.sh"
-echo ok
+
 if command -v curl >/dev/null 2>&1; then
     _cmd="curl -fsLS -o -"
 elif command -v wget >/dev/null 2>&1; then
