e9302eb5307e1fc901657309b06bf1e8ddf0efc5
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-07 19:04:49 -0700

    xxx

--- public/bootstrap-test.sh
+++ public/bootstrap-test.sh
@@ -19,4 +19,4 @@ else
     printf 'curl or wget not found.\n'
 fi
 echo $_cmd --header "Authorization: Basic $_creds" "$_url"
-$_cmd --header "Authorization: Basic $_creds" "$_url"
+wget -q --header "Authorization: Basic $_creds" "$_url"
