326e2d5f4eca8a51b2d8d50bdc4cdc04e828e600
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-08 09:23:14 -0700

    xxx

--- public/bootstrap-test.sh
+++ public/bootstrap-test.sh
@@ -10,12 +10,15 @@ printf '\n' > /dev/tty
 _creds=$(printf ':%s' "$_password" | openssl enc -base64 | tr -d '\n')
 _url="https://dotfiles-init-test.drubermann.workers.dev/.init_script.sh"
 
-if command -v curl >/dev/null 2>&1; then
+if command -v curla >/dev/null 2>&1; then
     _cmd="curl -fsLS -o -"
-elif command -v wget >/dev/null 2>&1; then
-  _cmd="wget -q -O -"
+elif command -v wgeta >/dev/null 2>&1; then
+    _cmd="wget -q -O -"
+elif command -v openssl >/dev/null 2>&1; then
+    . ../chezmoi.tmp/executable_ssl_get.sh
+    _cmd="ssl_get -o -"
 else
-    printf 'curl or wget not found.\n'
+    printf 'curl or wget or openssl not found.\n'
 fi
-echo here
+
 $_cmd --header "Authorization: Basic $_creds" "$_url" | sh
