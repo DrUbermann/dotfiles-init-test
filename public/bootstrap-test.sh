c1a573bc7e2c9fcde687b72ed0e44ce76030effe
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-07 18:41:11 -0700

    xxx

--- public/bootstrap-test.sh
+++ public/bootstrap-test.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
-echo hi
+## prompt for password without echoing it
+printf 'Password: ' > /dev/tty
+stty -echo < /dev/tty
+read -r _password < /dev/tty
+stty echo < /dev/tty
+printf '\n' > /dev/tty
 
-# ## prompt for password without echoing it
-# printf 'Password: ' > /dev/tty
-# stty -echo < /dev/tty
-# read -r _password < /dev/tty
-# stty echo < /dev/tty
-# printf '\n' > /dev/tty
+_creds=$(printf ':%s' "$_password" | base64)
+#_header="--header Authorization: Basic $_creds"
+_url="https://dotfiles-init-test.drubermann.workers.dev/.init_script.sh"
 
-# _creds=$(printf ':%s' "$_password" | base64)
-# #_header="--header Authorization: Basic $_creds"
-# _url="https://dotfiles-init-test.drubermann.workers.dev/.init_script.sh"
-
-# if command -v curl >/dev/null 2>&1; then
-#     _cmd="curl -fsLS"
-# elif command -v wget >/dev/null 2>&1; then
-#   _cmd="wget -q"
-# else
-#     printf 'curl or wget not found.\n'
-# fi
+if command -v curl >/dev/null 2>&1; then
+    _cmd="curl -fsLS"
+elif command -v wget >/dev/null 2>&1; then
+  _cmd="wget -q"
+else
+    printf 'curl or wget not found.\n'
+fi
  
-#  $_cmd --header "Authorization: Basic $_creds" "$_url" | sh
+ $_cmd --header "Authorization: Basic $_creds" "$_url" | sh
