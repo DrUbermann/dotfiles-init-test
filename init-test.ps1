dd675a3ab345973861ad8f2bb969908b71fa0cfb
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-10 13:09:53 -0700

    xxx

--- init-test.ps1
+++ init-test.ps1
@@ -6,7 +6,7 @@ Write-Output "Starting init-test.ps1" ##########################################
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
+#Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
 # Use integer value for TLS 1.2 to support .NET 4.0
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]3072
@@ -31,4 +31,4 @@ if (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock") 
 iex "&{$(irm 'https://get.chezmoi.io/ps1')} init --apply 'https://github.com/DrUbermann/dotfiles-init-test.git'"
 #$Env:LGR_LVL_CNSL = 0
 ## Start a new process so Developer Mode registry change will be read
-powershell.exe -ExecutionPolicy RemoteSigned -File "$HOME/chezmoi.tmp/init.ps1"
+powershell.exe -ExecutionPolicy Bypass -File "$HOME/chezmoi.tmp/init.ps1"
