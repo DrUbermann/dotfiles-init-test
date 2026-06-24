b65200db378b2fa8e30fdf4fe194fa46016fc957
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-24 15:25:16 -0700

    xxx

--- init-test.ps1
+++ init-test.ps1
@@ -50,12 +50,12 @@ foreach (`$path in `$paths) {
     $script = $MyInvocation.MyCommand.Path
     Write-Host "script = $script"
     $proc = Start-Process powershell.exe -PassThru -ArgumentList "-NoExit", "-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "$script"
-    #Start-Sleep -Seconds 10
-    #Write-Host "completed $script"
+    Start-Sleep -Seconds 10
+    Write-Host "completed $script"
     exit
 }
 
-#Write-Host 'after if ($PSVersionTable.CLRVersion.Major -lt 4)'
+Write-Host 'after if ($PSVersionTable.CLRVersion.Major -lt 4)'
 $client = New-Object System.Net.WebClient
 $client.Headers.Add('Authorization', "Basic $Env:creds")
 & ([scriptblock]::Create($client.DownloadString('https://dotfiles-init-test.drubermann.workers.dev/.excu/chezmoi-install-legacy.ps1'))) init --apply $url_dotfiles
