2e64662c9410c808e215f508bec53d4b86ba20f7
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-24 18:49:26 -0700

    xxx

--- chezmoi/chezmoi.tmp/executable_git_askpass.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-## Non-interactively pass credentials to git
-## Point environmental variable GIT_ASKPASS to this file
-
-case "$1" in
-  Username*) echo 51386141 ;;
-  Password*) echo "$GITHUB_PAT" ;;
-esac
