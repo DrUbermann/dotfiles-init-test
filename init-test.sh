9954b870d7c1ee0abce83dd8fb7f375b40355687
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-30 16:42:24 -0700

    xxx

--- init-test.sh
+++ init-test.sh
@@ -82,5 +82,6 @@ fi
 cd "$HOME" || exit 1
 
 sh -c "$($_cmd https://get.chezmoi.io)" sh init --apply https://github.com/DrUbermann/dotfiles-init-test.git
+
 #export LGR_LVL_CNSL=0 
 "$HOME/chezmoi.tmp/init.ps1" "$@"
