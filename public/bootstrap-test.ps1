56c253f9bd984f3e408035a926f6b98a7ccac260
Dr Ubermann <51386141+DrUbermann@users.noreply.github.com>
2026-06-30 16:37:26 -0700

    xxx

--- public/bootstrap-test.ps1
+++ public/bootstrap-test.ps1
@@ -15,4 +15,3 @@ $creds = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes(":$password"))
 $url = 'https://dotfiles-init-test.drubermann.workers.dev/init-test.ps1'
 
 Invoke-Expression ([System.Text.Encoding]::UTF8.GetString((Invoke-WebRequest -UseBasicParsing -Uri $url -Headers @{ Authorization = "Basic $creds" }).Content))
-
